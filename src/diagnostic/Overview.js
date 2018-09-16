c32812cd9570f27da58ee3589aaf73ecaf778fc4
Moxious <moxious@oldhat.org>
2018-09-16 17:37:31 -0400

    overview component first commit

---
+++ src/diagnostic/Overview.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import * as PropTypes from 'prop-types';
+import { Message } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import _ from 'lodash';
+
+class Overview extends Component {
+    state = {
+        name: null,
+        versions: null,
+        edition: null,
+        topology: null,
+        mode: null,
+        address: null,
+        user: null,
+    };
+
+    constructor(props, context) {
+        super(props, context);
+        this.driver = props.driver || context.driver;
+    }
+
+    getClusterStatus() {
+        const s1 = this.driver.session();
+        return s1.run('CALL dbms.cluster.overview()', {})
+            .then(results => {
+                console.log('CLUSTER RESULTS',results);
+                this.setState({ mode: 'CLUSTER', topology: results });
+            })
+            .catch(err => {
+                const str = `${err}`;
+                if (str.indexOf('no procedure') > -1) {
+                    this.setState({ mode: 'SINGLE', clusterInfo: null });
+                } else {
+                    console.error('CLUSTER ERROR', err);    
+                }
+            })            
+            .finally(() => s1.close());
+    }
+
+    getUser() {
+        const q = 'call dbms.showCurrentUser()';
+
+        const session = this.driver.session();
+        return session.run(q, {})
+            .then(results => {
+                const rec = results.records[0];
+                const user = {
+                    username: rec.get('username'),
+                    roles: rec.get('roles'),
+                    flags: rec.get('flags'),
+                };
+
+                this.setState({ user });
+            })
+            .catch(err => {
+                console.error('Failed to get user info', err);
+            })
+            .finally(() => session.close());
+    }
+
+    getAddress() {
+        const q = `
+            call dbms.listConfig() 
+            yield name, value 
+            where name='dbms.connectors.default_advertised_address' 
+            return value;
+        `;
+        const session = this.driver.session();
+        return session.run(q, {})
+            .then(results => {
+                const address = results.records[0].get('value');
+                this.setState({ address });
+            })
+            .catch(err => {
+                console.error('Failed to get node address', err);
+            })
+            .finally(() => session.close());
+    }
+
+    getDBComponents() {
+        const session = this.driver.session();
+        return session.run('CALL dbms.components()', {})
+            .then(results => {
+                const rec = results.records[0];
+
+                this.setState({
+                    name: rec.get('name'),
+                    versions: rec.get('versions'),
+                    edition: rec.get('edition'),
+                });
+            })
+            .catch(err => {
+                console.error('Failed to get DB components', err);
+            })
+            .finally(() => session.close());
+    }
+
+    componentDidMount() {
+        return Promise.all([
+            this.getDBComponents(), 
+            this.getClusterStatus(),
+            this.getAddress(),
+            this.getUser(),
+        ]);
+    }
+
+    render() {
+        return (
+            (this.state.name && this.state.mode) ? (
+                <div className='Overview'>
+                    <Message>
+                        <Message.Header>Neo4j {this.state.edition} version(s) {this.state.versions.join(', ')}</Message.Header>
+                        <ul>
+                            <li>Database is running in mode {this.state.mode} on {this.state.address}</li>
+                            <li>Halin is running under user&nbsp;
+                                {(_.get(this.state.user, 'username') || 'loading...')} 
+                                &nbsp;with roles&nbsp;
+                                {(_.get(this.state.user, 'roles') || ['(none)']).join(', ')}
+                                {
+                                    _.get(this.state.user, 'flags') ? (
+                                        ' and flags: ' + this.state.user.flags.join(', ')
+                                    ) : ''
+                                }
+                            </li>
+                        </ul>
+                    </Message>
+                </div>
+            ) : 'Loading...'
+        );
+    }
+}
+
+Overview.contextTypes = {
+    driver: PropTypes.object,
+};
+
+export default Overview;
