9648f11858a08b04223fc7aadfcfbdea6eea19e4
Moxious <moxious@oldhat.org>
2018-09-18 10:37:52 -0400

    reorganize state

--- src/performance/DBSize.js
+++ src/performance/DBSize.js
@@ -3,23 +3,23 @@ import CypherTimeseries from '../timeseries/CypherTimeseries';
 import uuid from 'uuid';
 
 class DBSize extends Component {
-    query = 'MATCH (n) RETURN count(n) as totalNodes';
-
-    displayColumns = [
-        { Header: 'Total Nodes', accessor: 'totalNodes'}, 
-    ];
-
+    state = {
+        key: uuid.v4(),
+        query: 'MATCH (n) RETURN count(n) as totalNodes',
+        displayColumns: [
+            { Header: 'Total Nodes', accessor: 'totalNodes'}, 
+        ],
+    };
+    
     render() {
-        const key = this.props.key || uuid.v4();
-
         return (
             <div className="DBSize">
                 <h3>Total Node Count</h3>
-                <CypherTimeseries key={key}
+                <CypherTimeseries key={this.state.key}
                     driver={this.props.driver}
                     node={this.props.node}
-                    query={this.query} 
-                    displayColumns={this.displayColumns}
+                    query={this.state.query} 
+                    displayColumns={this.state.displayColumns}
                     min={0}
                     />
             </div>
