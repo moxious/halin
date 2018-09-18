fa97c701f7b3b1f4a39e0fbdd4dc4ab88b2f87a7
Moxious <moxious@oldhat.org>
2018-09-18 14:05:44 -0400

    more diagnostics

--- src/data/HalinContext.js
+++ src/data/HalinContext.js
@@ -3,10 +3,14 @@ import ClusterNode from '../data/ClusterNode';
 import _ from 'lodash';
 import Promise from 'bluebird';
 import uuid from 'uuid';
+import moment from 'moment';
+import appPkg from '../package.json';
 
 const neo4j = require('neo4j-driver/lib/browser/neo4j-web.min.js').v1;
 
 export default class HalinContext {
+    domain = 'halin';
+
     constructor() {
         this.project = null;
         this.graph = null;
@@ -100,11 +104,76 @@ export default class HalinContext {
         }
     }
 
-    runDiagnostics() {
-        const data = [
-            { node: 'foo', domain: 'bar', key: 'baz', value: 'quux' },            
+    _nodeDiagnostics(clusterNode) {
+        const node = clusterNode.getAddress();
+        const mkEntry = (domain, key, value) =>
+            _.merge({ node }, { domain, key, value });
+
+        const basics = [
+            mkEntry(this.domain, 'protocols', clusterNode.protocols()),
+            mkEntry(this.domain, 'role', clusterNode.role),
+            mkEntry(this.domain, 'database', clusterNode.database),
+            mkEntry(this.domain, 'id', clusterNode.id),
         ];
 
-        return Promise.resolve(data);
+        const session = this.driverFor(clusterNode.getBoltAddress()).session();
+
+        // Query must return 'value'
+        const noFailCheck = (domain, query, key) =>
+            session.run(query, {})
+                .then(results => results.records[0])
+                .then(record => record.get('value'))
+                .catch(err => JSON.stringify(err))  // Convert errors into the value.
+                .then(value => mkEntry(domain, key, value));
+
+        // Format all JMX data into records.
+        const genJMX = session.run("CALL dbms.queryJmx('*:*')", {})
+            .then(results => 
+                results.records.map(rec =>
+                    mkEntry('jmx', rec.get('name'), JSON.stringify(rec.get('attributes')))));
+
+        // Format node config into records.
+        const genConfig = session.run('CALL dbms.listConfig()', {})
+            .then(results =>
+                results.records.map(rec => 
+                    mkEntry('config', rec.get('name'), rec.get('value'))))
+
+        const otherPromises = [
+            noFailCheck('algo', 'RETURN algo.version() as value', 'version'),
+            noFailCheck('apoc', 'RETURN apoc.version() as value', 'version'),
+            noFailCheck('nodes', 'MATCH (n) RETURN count(n) as value', 'count'),
+            noFailCheck('schema', 'call db.labels() yield label return collect(label) as value', 'labels'),
+        ];
+
+        return Promise.all([genJMX, genConfig, ...otherPromises])
+            .then(arrayOfArrays => _.flatten(arrayOfArrays))
+            .then(results => results.concat(basics))
+            .finally(() => session.close());
+    }
+
+    _halinDiagnostics() {
+        const mkEntry = (key, value) => 
+            _.merge({ domain: 'halin', node: 'n/a' }, { key, value });
+
+        const halinDrivers = Object.keys(this.drivers).map(uri => ({
+            domain: `${this.domain}-driver`,
+            node: uri, 
+            key: 'encrypted',
+            value: _.get(this.drivers[uri]._config, 'encrypted'),
+        }));
+
+        return Promise.resolve([
+            mkEntry('diagnosticsGenerated', moment.utc().toISOString()),
+            mkEntry('halinVersion', appPkg.version),
+        ].concat(halinDrivers));
+    }
+
+    runDiagnostics() {
+        const halinDiagPromises = this._halinDiagnostics();
+        const promises = this.clusterNodes.map(clusterNode => this._nodeDiagnostics(clusterNode));
+        promises.push(halinDiagPromises);
+
+        return Promise.all(promises)
+            .then(arrayOfArrays => _.flatten(arrayOfArrays));
     }
 }
