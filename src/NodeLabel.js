02afffd4ee002fa176240659e73fe6c626882d12
Moxious <moxious@oldhat.org>
2018-09-18 10:37:33 -0400

    first commit

---
+++ src/NodeLabel.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Image, Popup } from 'semantic-ui-react';
+
+/**
+ * A simple UI label that can be attached to other elements, to indicate which Neo4j node
+ * some data came from.
+ */
+export default class NodeLabel extends Component {
+    render() {
+        return (
+            <Popup trigger={
+                <div className="ui image label" style={{hover:'none'}}>
+                    <Image src="img/neo4j_logo_globe.png" size="mini"/>
+                    {this.props.node ? this.props.node.getAddress() : 'NONE' }
+                </div>
+            } content={
+                this.props.node ? this.props.node.getBoltAddress() : 'N/A'
+            }/>
+        )
+    }
+}
