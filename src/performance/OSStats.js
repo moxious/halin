6820a8ea8f1560e47eb4d5a1b8f84d25202a3767
Moxious <moxious@oldhat.org>
2018-09-29 21:03:20 -0400

    first complete commit

---
+++ src/performance/OSStats.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import Spinner from '../Spinner';
+import _ from 'lodash';
+import { Grid } from 'semantic-ui-react';
+import uuid from 'uuid';
+import AllocationChart from './AllocationChart';
+import datautil from '../data/util';
+
+export default class OSStats extends Component {
+    state = {
+        id: uuid.v4(),
+        rate: 1000,
+        pieWidth: 300,
+        pieHeight: 300,
+        query: `
+            CALL dbms.queryJmx("java.lang:type=OperatingSystem") 
+            YIELD attributes 
+            WITH
+                attributes.OpenFileDescriptorCount.value as fdOpen,
+                attributes.MaxFileDescriptorCount.value as fdMax,
+
+                attributes.FreePhysicalMemorySize.value as physFree,
+                attributes.TotalPhysicalMemorySize.value as physTotal,
+
+                attributes.CommittedVirtualMemorySize.value as virtCommitted,
+                attributes.FreeSwapSpaceSize.value as swapFree,
+                attributes.TotalSwapSpaceSize.value as swapTotal,
+
+                attributes.Name.value as osName,
+                attributes.Version.value as osVersion,
+                attributes.Arch.value as arch,
+                attributes.AvailableProcessors.value as processors
+            RETURN 
+                fdOpen, fdMax,
+                physFree, physTotal,
+                virtCommitted, swapFree, swapTotal,
+                osName, osVersion, arch, processors;
+        `,
+        displayColumns: [
+            { Header: 'Open FDs', accessor: 'fdOpen' },
+            { Header: 'Max FDs', accessor: 'fdMax' },
+            { Header: 'Physical Memory (Free)', accessor: 'physFree' },
+            { Header: 'Physical Memory (Total)', accessor: 'physTotal' },
+            { Header: 'Virtual Memory (Committed)', accessor: 'virtCommitted' },
+            { Header: 'Swap memory (Free)', accessor: 'swapFree' },
+            { Header: 'Swap memory (Total)', accessor: 'swapTotal' },
+            { Header: 'OS Name', accessor: 'osName' },
+            { Header: 'OS Version', accessor: 'osVersion' },
+            { Header: 'Arch', accessor: 'arch' },
+            { Header: 'Processors', accessor: 'processors' },
+        ],
+    }
+
+    componentDidMount() {
+        this.mounted = true;
+
+        this.feed = window.halinContext.getDataFeed({
+            node: this.props.node,
+            driver: this.props.driver,
+            query: this.state.query,
+            rate: this.state.rate,
+            windowWidth: 5000,
+            displayColumns: this.state.displayColumns,
+            params: {},
+        });
+
+        this.feed.onData = (newData, dataFeed) => {
+            // Don't need any of the timeseries stuff, just one data packet.
+            // console.log(newData.data[0]);
+
+            const data = _.cloneDeep(newData.data[0]);
+            if (this.mounted) {
+                this.setState({ data });
+            }
+        }
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+    }
+
+    labelFor = key => {
+        return _.get(this.state.displayColumns.filter(i => i.accessor === key)[0], 'Header') || key;
+    }
+
+    format = (key, value) => {
+        const dataSizes = ['physFree', 'physTotal', 'virtCommitted',
+            'swapFree', 'swapTotal'];
+
+        if (dataSizes.indexOf(key) > -1) {
+            return datautil.humanDataSize(value);
+        }
+
+        return value;
+    }
+
+    render() {
+        const os = _.get(this.state.data, 'osName') || 'unknown';
+        const version = _.get(this.state.data, 'osVersion') || 'unknown';
+        const arch = _.get(this.state.data, 'arch') || 'unknown';
+        const procs = _.get(this.state.data, 'processors') || 'unknown'
+
+        const header = `${os} v${version} (${arch} architecture) with ${procs} cores`;
+
+        return this.state.data ? (
+            <div className='OSStats' key={this.state.id}>
+                <h4>{header}</h4>
+
+                <Grid className='OSStats' key={this.state.id}>
+                    <Grid.Row columns={3}>
+                        <Grid.Column>
+                            <AllocationChart
+                                dataMeasurement={true}
+                                free={this.state.data.physFree}
+                                total={this.state.data.physTotal}
+                                label="Physical Memory"
+                            />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <AllocationChart
+                                dataMeasurement={true}
+                                free={this.state.data.swapFree}
+                                total={this.state.data.swapTotal}
+                                label="Swap Memory"
+                            />
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <AllocationChart
+                                free={this.state.data.fdOpen}
+                                total={this.state.data.fdMax}
+                                label="File Descriptors"
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        ) : <Spinner active={true} />;
+    }
+};
