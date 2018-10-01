22c5a6f097838ec4920b8407cd86fe856262f62b
Moxious <moxious@oldhat.org>
2018-10-01 15:01:36 -0400

    first commit

---
+++ src/diagnostic/ConfigurationDiff.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import diffutil from '../data/diff/index';
+import ReactTable from 'react-table';
+import { Icon } from 'semantic-ui-react';
+import _ from 'lodash';
+
+export default class ConfigurationDiff extends Component {
+    state = {
+        diff: null,
+        displayColumns: null,
+        labels: null,
+    };
+
+    componentDidMount() {
+        const setOfSets = {};
+        const displayColumns = [
+            { 
+                Header: 'Configuration Item', 
+                accessor: 'name',
+                Cell: props => 
+                    <div className='Neo4jConfig_Name'>
+                        <a 
+                            target='neo4jConfig' 
+                            href={this.baseURL + props.value}>
+                            {props.value}
+                        </a>                    
+                    </div>,
+            },
+            {
+                Header: 'All Values Match', 
+                accessor: 'unanimous',
+                filterMethod: (filter, row) => {
+                    if (filter.value === "all") {
+                        return true;
+                    }
+                    
+                    if (filter.value === 'true') {
+                        return row[filter.id];
+                    } else { return !row[filter.id] }
+                },
+                Filter: ({ filter, onChange }) =>
+                    <select
+                        onChange={event => onChange(event.target.value)}
+                        style={{ width: "100%" }}
+                        value={filter ? filter.value : "all"}
+                    >
+                        <option value="all">All</option>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>,
+                Cell: props => props.value ? 
+                    <Icon circular color='green' name='checkmark'/> : 
+                    <Icon circular color='red' name='cancel' />,
+            },
+        ];
+
+        let idx = 0;
+
+        this.props.data.nodes.forEach(node => {
+            const label = node.basics.role + (idx++);
+            const addr = node.basics.address;
+
+            setOfSets[label] = _.cloneDeep(node.configuration);
+            displayColumns.push({
+                Header: addr,
+                accessor: label,
+                style: { whiteSpace: 'unset' },
+            });
+        });
+
+        const diff = diffutil.configurationDiff(setOfSets);
+        console.log('DIFF', diff, displayColumns);
+        this.setState({ diff, displayColumns });
+    }
+
+    render() {
+        if (!this.state.diff) {
+            return (
+                <div className='ConfigurationDiff'>
+                    <h4>Configuration Diff</h4>
+
+                    No data specified.
+                </div>
+            );
+        }
+
+        console.lo
+        return (
+            <div className='ConfigurationDiff'>
+                <h4>Configuration Diff</h4>
+
+                <ReactTable
+                    // By default, filter only catches data if the value STARTS WITH
+                    // the entered string.  This makes it less picky.
+                    defaultFilterMethod={(filter, row, column) => {
+                        const id = filter.pivotId || filter.id
+                        return row[id] !== undefined ? String(row[id]).indexOf(filter.value) > -1 : true
+                    }}
+                    columns={this.state.displayColumns}
+                    data={this.state.diff.table}
+                    sortable={true}
+                    filterable={true}
+                    defaultPageSize={20}
+                    showPagination={true}
+                />
+
+                I'm here! <pre style={{textAlign: 'left', maxWidth:800}}>{JSON.stringify(this.state.diff, null, 2)}</pre>
+            </div>
+        )
+    }
+}
