e3147a725f0186fb38dadadac51e0b2e6eb4142b
Moxious <moxious@oldhat.org>
2018-10-02 19:53:15 -0400

    queryLibrary refactor

--- src/overview/TransactionsOpen.js
+++ src/overview/TransactionsOpen.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 import ClusterTimeseries from '../timeseries/ClusterTimeseries';
 import uuid from 'uuid';
+import queryLibrary from '../data/query-library';
 
 class TransactionsOpen extends Component {
     state = {
         key: uuid.v4(),
         rate: 2000,
         width: 400,
-        query: `
-            CALL dbms.queryJmx("org.neo4j:instance=kernel#0,name=Transactions") 
-            YIELD attributes WITH attributes as a 
-            RETURN 
-                a.NumberOfOpenTransactions.value as open
-        `,
-
-        displayColumns: [
-            { Header: 'Open', accessor: 'open' },
-        ],
-        legendOnlyColumns: [
-            { Header: 'Peak Concurrent', accessor: 'concurrent' },
-            { Header: 'Opened', accessor: 'opened' },
-            { Header: 'Committed', accessor: 'committed' },
-            { Header: 'Last Committed', accessor: 'lastCommittedId' },
-        ],
+        query: queryLibrary.JMX_TRANSACTIONS.query,
     };
 
     onUpdate = (childQueryState) => {
--- src/performance/MemoryMonitor.js
+++ src/performance/MemoryMonitor.js
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
 import CypherTimeseries from '../timeseries/CypherTimeseries';
 import uuid from 'uuid';
+import queryLibrary from '../data/query-library';
 
 class MemoryMonitor extends Component {
     state = {
         key: uuid.v4(),
         rate: 1000,
         width: 400,
-        query: `
-            CALL dbms.queryJmx('java.lang:type=Memory') yield attributes 
-            WITH 
-                attributes.HeapMemoryUsage as heap, 
-                attributes.NonHeapMemoryUsage as nonHeap
-
-            WITH 
-                heap.value.properties as heapProps,
-                nonHeap.value.properties as nonHeapProps
-            
-            return 
-                heapProps.init as heapInit, 
-                heapProps.committed as heapCommitted,
-                heapProps.used as heapUsed, 
-                heapProps.max as heapMax,
-                nonHeapProps.init as nonHeapInit,
-                nonHeapProps.committed as nonHeapCommitted,
-                nonHeapProps.used as nonHeapUsed,
-                nonHeapProps.max as nonHeapMax,
-                heapProps.used + nonHeapProps.used as totalMem
-        `,
-
-        displayColumns: [
-            { Header: 'Total Memory', accessor: 'totalMem' },
-            { Header: 'Heap Used', accessor: 'heapUsed' },
-            { Header: 'Heap Committed', accessor: 'heapCommitted' },
-            { Header: 'Nonheap Used', accessor: 'nonHeapUsed' },
-        ],
+        query: queryLibrary.JMX_MEMORY_STATS.query,
+        displayColumns: queryLibrary.JMX_MEMORY_STATS.columns,
     };
 
     render() {
