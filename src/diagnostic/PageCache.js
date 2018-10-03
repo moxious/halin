eea2a70e4ba9d6da62787c59e082d72042907c09
Moxious <moxious@oldhat.org>
2018-10-02 20:11:41 -0400

    queryLibrary refactoring

--- src/diagnostic/PageCache.js
+++ src/diagnostic/PageCache.js
@@ -1,45 +1,13 @@
 import React, { Component } from 'react';
 import * as PropTypes from 'prop-types';
 import CypherDataTable from '../data/CypherDataTable';
-
-const cdt = CypherDataTable; // alias for shorthand
+import queryLibrary from '../data/query-library';
 
 class PageCache extends Component {
     state = {
         rate: 2000,
-        query: `
-            CALL dbms.queryJmx('org.neo4j:instance=kernel#0,name=Page cache')
-            YIELD attributes 
-            WITH 
-                attributes.Faults.value as faults, 
-                attributes.EvictionExceptions.value as evictionExceptions, 
-                attributes.BytesWritten.value as bytesWritten, 
-                attributes.Flushes.value as flushes, 
-                attributes.UsageRatio.value as usageRatio, 
-                attributes.Evictions.value as evictions, 
-                attributes.FileUnmappings.value as fileUnmappings, 
-                attributes.BytesRead.value as bytesRead, 
-                attributes.FileMappings.value as fileMappings, 
-                attributes.HitRatio.value as hitRatio 
-            RETURN 
-                hitRatio, bytesRead, fileMappings, fileUnmappings,
-                flushes, usageRatio, bytesWritten, 
-                faults, evictions, evictionExceptions;        
-        `,
-        displayColumns: [
-            { Header: 'Usage Ratio', accessor: 'usageRatio', Cell: cdt.pctField },
-            { Header: 'Hit Ratio', accessor: 'hitRatio', Cell: cdt.pctField },
-            { Header: 'Read', accessor: 'bytesRead', Cell: cdt.dataSizeField },
-            { Header: 'Written', accessor: 'bytesWritten', Cell: cdt.dataSizeField },
-
-            { Header: 'Faults', accessor: 'faults', Cell: cdt.numField },            
-            { Header: 'Flushes', accessor: 'flushes', Cell: cdt.numField, show: false },
-            { Header: 'Evictions', accessor: 'evictions', Cell: cdt.numField, show: false },
-
-            { Header: 'Eviction Except.', accessor: 'evictionExceptions', Cell: cdt.numField, show: false },
-            { Header: 'File Mappings', accessor: 'fileMappings', Cell: cdt.numField, show: false },
-            { Header: 'File Unmappings', accessor: 'fileUnmappings', Cell: cdt.numField, show: false },
-        ],
+        query: queryLibrary.JMX_PAGE_CACHE.query,
+        displayColumns: queryLibrary.JMX_PAGE_CACHE.columns,
     };
 
     constructor(props, context) {
