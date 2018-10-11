18398a36bc8dc1c059711392aebfabe277d5e364
Moxious <moxious@oldhat.org>
2018-10-11 18:54:28 -0400

    ActiveTransactions component as complement to ActiveQueries

--- src/data/query-library.js
+++ src/data/query-library.js
@@ -170,4 +170,30 @@ export default {
             { Header: 'Processors', accessor: 'processors' },
         ],
     },
+
+    LIST_TRANSACTIONS: {
+        query: 'call dbms.listTransactions()',
+        columns: [
+            { Header: 'ID', accessor: 'transactionId' },
+            { Header: 'User', accessor: 'username' },
+            { Header: 'Metadata', accessor: 'metaData', show: false, Cell: cdt.jsonField },
+            { Header: 'Start Time', accessor: 'startTime', show: false },
+            { Header: 'Protocol', accessor: 'protocol', show: false },
+            { Header: 'Client Addr', accessor: 'clientAddress', show: false },
+            { Header: 'Request URI', accessor: 'requestUri', show: false },
+            { Header: 'QueryId', accessor: 'currentQueryId', show: false },
+            { Header: 'Query', accessor: 'currentQuery', style: { whiteSpace: 'unset', textAlign: 'left' } },
+            { Header: 'Locks', accessor: 'activeLockCount', Cell: cdt.numField },
+            { Header: 'Status', accessor: 'status' },
+            { Header: 'Resource Info', accessor: 'resourceInformation', show: false, Cell: cdt.jsonField },
+            { Header: 'Idle', accessor: 'idleTimeMillis', Cell: cdt.timeField, show: false },
+            { Header: 'Wait', accessor: 'waitTimeMillis', Cell: cdt.timeField },
+            { Header: 'CPU', accessor: 'cpuTimeMillis', Cell: cdt.timeField, show: false },
+            { Header: 'Elapsed', accessor: 'elapsedTimeMillis', Cell: cdt.timeField },
+            { Header: 'Bytes', accessor: 'allocatedBytes', Cell: cdt.numField, show: false },
+            { Header: 'Direct (b)', accessor: 'allocatedDirectBytes', Cell: cdt.numField, show: false },            
+            { Header: 'PageHits', accessor: 'pageHits', Cell: cdt.numField },
+            { Header: 'PageFaults', accessor: 'pageFaults', Cell: cdt.numField },
+        ],
+    }
 };
