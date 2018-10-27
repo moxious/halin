35fd73b7609f3eb0b556f752b4404d0c9004780b
Moxious <moxious@oldhat.org>
2018-10-27 15:59:21 -0400

    refactor disk usage query -> queryLibrary

--- src/data/query-library.js
+++ src/data/query-library.js
@@ -13,6 +13,43 @@ export default {
         windowWidth: 10 * 1000,
     },
 
+    JMX_STORE_SIZES: {
+        query: `
+            CALL dbms.queryJmx('org.neo4j:instance=kernel#0,name=Store sizes') 
+            YIELD attributes 
+            WITH
+                attributes.CountStoreSize.value as countStore,
+                attributes.IndexStoreSize.value as indexStore,
+                attributes.LabelStoreSize.value as labelStore,
+                attributes.NodeStoreSize.value as nodeStore,
+                attributes.PropertyStoreSize.value as propStore, 
+                attributes.RelationshipStoreSize.value as relStore, 
+                attributes.SchemaStoreSize.value as schemaStore,
+                attributes.StringStoreSize.value as stringStore, 
+                attributes.TotalStoreSize.value as total, 
+                attributes.TransactionLogsSize.value as txLogs,                 
+                attributes.ArrayStoreSize.value as arrayStore
+            RETURN 
+                countStore, indexStore, labelStore,
+                schemaStore, txLogs,
+                stringStore, arrayStore, 
+                relStore, propStore, total, nodeStore;        
+        `,
+
+        columns: [
+            { Header: 'Total Disk', accessor: 'total' },
+            { Header: 'Nodes', accessor: 'nodeStore' },
+            { Header: 'Rels', accessor: 'relStore' },
+            { Header: 'Props', accessor: 'propStore' },
+            { Header: 'Index', accessor: 'indexStore' },
+            { Header: 'Schema', accessor: 'schemaStore' },
+            { Header: 'TXs', accessor: 'txLogs' },
+            { Header: 'Strings', accessor: 'stringStore' },
+            { Header: 'Arrays', accessor: 'arrayStore' },            
+        ],
+        rate: 1000,
+    },
+
     JMX_PAGE_CACHE: {
         query: `
         CALL dbms.queryJmx('org.neo4j:instance=kernel#0,name=Page cache')
--- src/performance/DiskUsage.js
+++ src/performance/DiskUsage.js
@@ -1,59 +1,28 @@
 import React, { Component } from 'react';
 import CypherTimeseries from '../timeseries/CypherTimeseries';
 import uuid from 'uuid';
+import queryLibrary from '../data/query-library';
 
 class DiskUsage extends Component {
     state = {
         key: uuid.v4(),
-        rate: 1000,
         width: 400,
-        query: `
-            CALL dbms.queryJmx('org.neo4j:instance=kernel#0,name=Store sizes') 
-            YIELD attributes 
-            WITH
-                attributes.CountStoreSize.value as countStore,
-                attributes.IndexStoreSize.value as indexStore,
-                attributes.LabelStoreSize.value as labelStore,
-                attributes.NodeStoreSize.value as nodeStore,
-                attributes.PropertyStoreSize.value as propStore, 
-                attributes.RelationshipStoreSize.value as relStore, 
-                attributes.SchemaStoreSize.value as schemaStore,
-                attributes.StringStoreSize.value as stringStore, 
-                attributes.TotalStoreSize.value as total, 
-                attributes.TransactionLogsSize.value as txLogs,                 
-                attributes.ArrayStoreSize.value as arrayStore
-            RETURN 
-                countStore, indexStore, labelStore,
-                schemaStore, txLogs,
-                stringStore, arrayStore, 
-                relStore, propStore, total, nodeStore;        
-        `,
-
-        displayColumns: [
-            { Header: 'Total Disk', accessor: 'total' },
-            { Header: 'Nodes', accessor: 'nodeStore' },
-            { Header: 'Rels', accessor: 'relStore' },
-            { Header: 'Props', accessor: 'propStore' },
-            { Header: 'Index', accessor: 'indexStore' },
-            { Header: 'Schema', accessor: 'schemaStore' },
-            { Header: 'TXs', accessor: 'txLogs' },
-            { Header: 'Strings', accessor: 'stringStore' },
-            { Header: 'Arrays', accessor: 'arrayStore' },            
-        ],
     };
 
     render() {
+        const q = queryLibrary.JMX_STORE_SIZES;
+
         return (
             <div className="DiskUsage">
                 <CypherTimeseries key={this.state.key}
                     driver={this.props.driver}
                     node={this.props.node}
-                    query={this.state.query}
+                    query={q.query}
                     width={this.state.width}
-                    rate={this.state.rate}
-                    displayColumns={this.state.displayColumns}
+                    rate={q.rate}
+                    displayColumns={q.columns}
                     startingEnabled={[
-                        this.state.displayColumns[0]
+                        q.columns[0]
                     ]}
                 />
             </div>
