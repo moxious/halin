27809ec949430a6cd251398178534c427c4a490a
Moxious <moxious@oldhat.org>
2018-11-07 08:18:00 -0500

    reformats troubleshooting info and fixes #33

--- src/neo4jDesktop/Troubleshooting.js
+++ src/neo4jDesktop/Troubleshooting.js
@@ -2,33 +2,50 @@ import React, { Component } from 'react';
 import { Message } from 'semantic-ui-react';
 
 export default class Troubleshooting extends Component {
+    issuesAndResolutions = [
+        {
+            detector: /unauthorized/i, 
+            suggestions: ['Double check your username and password and try again'],
+        },
+        {
+            detector: /failed to establish connection in/i,
+            suggestions: [`This error may mean that your database instance is under heavy load or is
+            not currently responsive`],
+        },
+        {
+            detector: /security constraints in your web browser/i,
+            suggestions: [
+                `This error often means that you have untrusted SSL certificates on your server.
+                Either install trusted certificates, or try again without encryption.`,
+                'This error can also mean that you have provided an incorrect DNS name or address for the database',
+                'If you are using Neo4j Desktop and running on your local machine, de-select encryption.',            
+                'Double check your host and try again'
+            ],
+        },
+        {
+            detector: /./,
+            suggestions: [`
+            Unfortunately, no troubleshooting is available for this particular error. 
+            Consider checking the Neo4j community site for more information.`],
+        },
+    ];
+
     renderList(suggestions) {
         if (!suggestions) { return ''; }
         return suggestions.map((someText, idx) => <p key={idx}>{someText}</p>);
     }
 
     render() {
-        const str = `${this.props.err}`;
-        let suggestions = null;
+        const str = `${this.props.error}`;
+        let suggestions = ['No suggestions available'];
 
-        if (str.indexOf('unauthorized') > -1) {
-            suggestions = ['Double check your username and password and try again'];
-        } else if(str.indexOf('Failed to establish connection in') > -1) {
-            suggestions = [
-                `This error may mean that your database instance is under heavy load or is
-                not currently responsive`,
-            ];
-        } else if(str.indexOf('security constraints in your web browser') > -1) {
-            suggestions = [`
-                This error often means that you have untrusted SSL certificates on your server.
-                Either install trusted certificates, or try again without encryption.`,
-                'If you are using Neo4j Desktop and running on your local machine, de-select encryption',
-            ];
-        } else {
-            suggestions = [`
-                Unfortunately, no troubleshooting is available for this particular error. 
-                Consider checking the Neo4j community site for more information.
-            `];
+        for (let i=0; i<this.issuesAndResolutions.length; i++) {
+            const option = this.issuesAndResolutions[i];
+
+            if (str.match(option.detector)) {
+                suggestions = option.suggestions;
+                break;
+            }
         }
 
         return (
