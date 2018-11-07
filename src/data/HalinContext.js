d2fec611b228710f08eb3fab404cb6f510080b84
Moxious <moxious@oldhat.org>
2018-11-07 09:11:38 -0500

    fixes #27 detect native auth in clusters and selectively disable user management

--- src/data/HalinContext.js
+++ src/data/HalinContext.js
@@ -32,6 +32,7 @@ export default class HalinContext {
             connectionTimeout: 10000,
             trust: 'TRUST_CUSTOM_CA_SIGNED_CERTIFICATES',
         };
+        this.dbms = {};
         this.debug = true;
         this.mgr = new ClusterManager(this);
     }
@@ -106,6 +107,13 @@ export default class HalinContext {
      */
     isEnterprise() {
         return this.dbms.edition === 'enterprise';
+    }
+
+    /**
+     * Returns true if the context provides for native auth management, false otherwise.
+     */
+    supportsNativeAuth() {
+        return this.dbms.nativeAuth;
     }
 
     /**
@@ -228,24 +236,49 @@ export default class HalinContext {
         const q = 'call dbms.components()';
         const session = driver.session();
 
-        return session.run(q, {})
+        const componentsPromise = session.run(q, {})
             .then(results => {
                 const rec = results.records[0];
-                this.dbms = {
-                    name: rec.get('name'),
-                    versions: rec.get('versions'),
-                    edition: rec.get('edition'),
-                };
+                this.dbms.name = rec.get('name')
+                this.dbms.versions = rec.get('versions');
+                this.dbms.edition = rec.get('edition');
             })
             .catch(err => {
                 Sentry.captureException(err);
                 console.error('Failed to get DBMS components');
-                this.dbms = {
-                    name: 'UNKNOWN',
-                    versions: [],
-                    edition: 'UNKNOWN',
-                };
+                this.dbms.name = 'UNKNOWN';
+                this.dbms.versions = [];
+                this.dbms.edition = 'UNKNOWN';
             });
+
+        // See issue #27 for what's going on here.  DB must support native auth
+        // in order for us to expose some features, such as user management.
+        const authQ = `
+            CALL dbms.listConfig() YIELD name, value 
+            WHERE name =~ 'dbms.security.auth_provider.*' 
+            RETURN value;`;
+        const authPromise = session.run(authQ, {})
+            .then(results => {
+                let nativeAuth = false;
+
+                results.records.forEach(rec => {
+                    const val = rec.get('value');
+                    const valAsStr = `${val}`; // Coerce ['foo','bar']=>'foo,bar' if present
+                    
+                    if (valAsStr.indexOf('native') > -1) {
+                        nativeAuth = true;
+                    }
+                });
+
+                this.dbms.nativeAuth = nativeAuth;
+            })
+            .catch(err => {
+                Sentry.captureException(err);
+                console.error('Failed to get DBMS auth implementation type');
+                this.dbms.nativeAuth = false;
+            });
+
+        return Promise.all([componentsPromise, authPromise]);
     }
 
     checkUser(driver) {
