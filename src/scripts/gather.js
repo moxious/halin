d0fd40fbc4770fcdd64178230c75126037fb720e
Moxious <moxious@oldhat.org>
2018-11-29 17:15:41 -0800

    gather diagnostics and quit

--- src/scripts/gather.js
+++ src/scripts/gather.js
@@ -4,9 +4,21 @@ import yargs from 'yargs';
 console.log(yargs.argv);
 const ctx = new HalinContext();
 
+const gatherDiagnosticsAndQuit = (halin) => {
+    return halin.runDiagnostics()
+        .then(data => {
+            console.log(JSON.stringify(data, null, 2));
+            return halin.shutdown();
+        })
+        .then(() => process.exit(0));
+};
+
 ctx.initialize()
     .then(ctx => {
         console.log('Good:',ctx);
+
+        setTimeout(() => gatherDiagnosticsAndQuit(ctx), 
+            process.env.WAIT_TIME || 5000);
     })
     .catch(err => {
         console.error('ZOMG',err);
