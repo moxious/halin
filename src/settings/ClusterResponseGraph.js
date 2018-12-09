4860b168be83e543a01ab2115f892270c646843e
Moxious <moxious@oldhat.org>
2018-12-09 15:41:26 -0500

    added cluster response scattergraph for debugging

---
+++ src/settings/ClusterResponseGraph.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { ScatterPlot } from 'react-d3-components';
+
+export default class ClusterResponseGraph extends Component {
+    state = {
+        dataSeries: null,
+    };
+
+    getData() {
+        if (!this.mounted) { return null; } 
+        const ctx = window.halinContext;
+
+        const dataSeries = ctx.clusterNodes.map(node => ({
+            label: node.getLabel(),
+            values: node.getObservations().toArray(),
+        }));
+
+        this.setState({
+            dataSeries,
+        });
+    }
+
+    componentDidMount() {
+        this.mounted = true;
+        this.interval = setInterval(() => this.getData(), 750);
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+        clearInterval(this.interval);
+    }
+
+    render() {
+        const tooltipScatter = (x, y, rest) => {
+            return `${x}: ${y}ms`;
+        };
+
+        return (
+            <div className='ClusterResponseGraph'>{ this.state.dataSeries ? 
+                <ScatterPlot 
+                    data={this.state.dataSeries}
+                    width={600}
+                    height={300}
+                    margin={{top: 10, bottom: 50, left: 50, right: 10}}
+                    tooltipHtml={tooltipScatter}
+                    xAxis={{label: "Time"}}
+                    yAxis={{label: "Response time (ms)"}}/>
+                : '' }
+            </div>
+        );
+    }
+}
--- src/settings/SettingsPane.js
+++ src/settings/SettingsPane.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import DataFeedStats from './DataFeedStats';
+import ClusterResponseGraph from './ClusterResponseGraph';
+import './SettingsPane.css';
+
+const debug = false;
 
 export default class SettingsPane extends Component {
     render() {
         // const style = {textAlign:'left'};
         return (
             <div className='SettingsPane'>
+                { debug ? <ClusterResponseGraph/> : '' }
+
                 <DataFeedStats />
             </div>
         )
