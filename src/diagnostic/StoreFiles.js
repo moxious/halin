b34124060d55948743a5f893a098ae01eae87134
Moxious <moxious@oldhat.org>
2019-01-11 10:22:14 -0500

    fixes #54 ; adds "Other" column to store file sizes in graph.db folder

--- src/diagnostic/StoreFiles.js
+++ src/diagnostic/StoreFiles.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react';
 import fields from '../data/fields';
 import CypherDataTable from '../data/CypherDataTable';
+import queryLibrary from '../data/query-library';
 
 class StoreFiles extends Component {
     state = {
         rate: (1000 * 60), // Once per minute
-        query: `
-            CALL dbms.queryJmx('org.neo4j:instance=kernel#0,name=Store file sizes') 
-            YIELD attributes 
-            WITH
-                attributes.LogicalLogSize.value as logicalLog, 
-                attributes.StringStoreSize.value as stringStore, 
-                attributes.ArrayStoreSize.value as arrayStore, 
-                attributes.RelationshipStoreSize.value as relStore, 
-                attributes.PropertyStoreSize.value as propStore, 
-                attributes.TotalStoreSize.value as total, 
-                attributes.NodeStoreSize.value as nodeStore
-            RETURN 
-                logicalLog, stringStore, arrayStore, 
-                relStore, propStore, total, nodeStore;        
-        `,
+        query: queryLibrary.JMX_STORE_SIZES.query,
         displayColumns: [
             { Header: 'Total', accessor: 'total', Cell: fields.dataSizeField },
             { Header: 'Nodes', accessor: 'nodeStore', Cell: fields.dataSizeField },
             { Header: 'Relationships', accessor: 'relStore', Cell: fields.dataSizeField },
             { Header: 'Properties', accessor: 'propStore', Cell: fields.dataSizeField },
 
-            { Header: 'Logical Log', accessor: 'logicalLog', Cell: fields.dataSizeField, show: false },
+            { Header: 'TX Logs', accessor: 'txLogs', Cell: fields.dataSizeField, show: false },
             { Header: 'Strings', accessor: 'stringStore', Cell: fields.dataSizeField, show: false },
             { Header: 'Arrays', accessor: 'arrayStore', Cell: fields.dataSizeField, show: false },
+            { Header: 'Other', accessor: 'otherStore', Cell: fields.dataSizeField, show: true },
         ],
     };
 
