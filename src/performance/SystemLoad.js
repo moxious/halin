84549738ce2b7efc7ff0bea794202b98ca7a714e
Moxious <moxious@oldhat.org>
2019-02-21 10:36:07 -0500

    explainers

--- src/performance/SystemLoad.js
+++ src/performance/SystemLoad.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CypherTimeseries from '../timeseries/CypherTimeseries';
 import uuid from 'uuid';
 import queryLibrary from '../data/query-library';
+import Explainer from '../Explainer';
 
 class SystemLoad extends Component {
     state = {
@@ -12,11 +13,27 @@ class SystemLoad extends Component {
         displayColumns: queryLibrary.OS_LOAD_STATS.columns,
     };
 
+    help() {
+        return (
+            <div className='SystemLoadHelp'>
+                <p>The system load is a measure of the amount of computational work that a computer system performs.</p>
+                <p>System load represents overall load, while process load shows how much load is caused by
+                    the Neo4j process itself.
+                </p>
+
+                <p><a href="https://neo4j.com/docs/operations-manual/current/performance/">Read more about Neo4j performance tuning</a></p>
+            </div>
+        );
+    }
+
     render() {
+        const explainer = <Explainer content={this.help()}/>;
+
         return (
             <div className="SystemLoad">
                 <CypherTimeseries key={this.state.key}
                     driver={this.props.driver}
+                    explainer={explainer}
                     node={this.props.node}
                     query={this.state.query}
                     width={this.state.width}
