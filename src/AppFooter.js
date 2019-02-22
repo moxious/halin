561a02f8e231740bc78ada90eba858a9e63bbbb1
Moxious <moxious@oldhat.org>
2019-02-22 18:48:26 -0500

    fullscreen halin settings modal with close icon

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -12,7 +12,7 @@ export default class AppFooter extends Component {
                 &nbsp;|&nbsp;
                 <a target='halin' href='https://neo4j.com/docs/operations-manual/current/'>Neo4j Operations Manual</a>
                 &nbsp;&nbsp;
-                <Modal 
+                <Modal size='fullscreen' closeIcon
                     trigger={<Button icon='settings'/>}>
                     <Header>Halin Internal Settings</Header>
                     <Modal.Content scrolling>
