91ef9112b03fb762d3f0a421fa32be83b3f18ce2
Moxious <moxious@oldhat.org>
2019-05-11 17:57:00 -0400

    handle nested JSON arrays and objects

--- src/data/download/CSVDownload.js
+++ src/data/download/CSVDownload.js
@@ -9,7 +9,14 @@ import neo4j from '../../driver/index';
 // and then safely escape that string for CSV
 const toCsvString = obj => {
     const intermed = neo4j.handleNeo4jInt(obj);
-    return _.isString(intermed) ? intermed : JSON.stringify(intermed);
+    const asString = _.isString(intermed) ? intermed : JSON.stringify(intermed);
+    
+    // It sucks, but react-csv does not escape double quotes for us, so 
+    // JSON objects and arrays being written as strings get hosed unless
+    // you CSV escape the " characters, which is done with double "" and
+    // NOT with \"
+    // https://stackoverflow.com/questions/46637955/write-a-string-containing-commas-and-double-quotes-to-csv
+    return asString.replace(/"/g, '""');
 };
 
 export default class CSVDownload extends Component {
@@ -23,15 +30,23 @@ export default class CSVDownload extends Component {
     }
 
     render() {
+        // User may specify to includeHidden, which dumps to CSV columns that
+        // the user can't necessarily see, but which are in the data.
+        const shouldShowColumn = col => {
+            if (this.props.includeHidden) { return true; }
+
+            // Following ReactTable rules, show is default true, but can be
+            // toggled off if user sets show=false.
+            return _.isNil(col.show) || col.show;
+        };
+
         // Pull out display columns, only those with accessors.
         // These are our data fields.  This intentionally skips things like
         // virtual columns which have a Cell renderer defined, but no accessor,
         // meaning that there won't be any data available for that column.
         const accessible = this.props.displayColumns
             .filter(col => col.accessor)
-            // Following ReactTable rules, show is default true, but can be
-            // toggled off if user sets show=false.
-            .filter(col => _.isNil(col.show) || col.show);
+            .filter(shouldShowColumn);
 
         const data = this.props.data.map(obj => {
             // Each object has to be turned into a simple array,
