7326713849f715427f5984dba1c15cd883338b00
Moxious <moxious@oldhat.org>
2019-05-11 17:08:11 -0400

    data is filtered by parent component

--- src/db/queries/QueryStatTable.js
+++ src/db/queries/QueryStatTable.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
-import HalinQuery from '../../data/queries/HalinQuery';
 
 export default class QueryStatTable extends Component {
     render() {
-        const filterData = () => (
-            this.props.includeHalinQueries ? 
-            this.props.data : 
-            this.props.data.filter(i => !HalinQuery.isDisclaimed(i.query)));
-
         return (
             <ReactTable
                 defaultFilterMethod={(filter, row, column) => {
@@ -17,7 +11,7 @@ export default class QueryStatTable extends Component {
                 }}
                 sortable={true}
                 filterable={true}
-                data={filterData(this.props.data)}
+                data={this.props.data}
                 showPagination={true}
                 defaultPageSize={Math.min(this.props.data.length, 10)}
                 className="-striped -highlight"
