136ae0045b27e9558c4f97212e1402885af2f03f
Moxious <moxious@oldhat.org>
2019-05-13 15:14:03 -0400

    test for date parsing

--- src/data/logs/LogEvent.test.js
+++ src/data/logs/LogEvent.test.js
@@ -1,16 +1,19 @@
 import LogEvent from './LogEvent';
+import moment from 'moment';
 
 describe('Log Events', function() {
     const cls = 'o.n.g.f.m.e.CommunityEditionModule';
     const log = 'INFO';
+    const date1 = '2019-04-23 19:16:27.905+0000';
+    const date2 = '2019-04-25 19:16:27.905+0000';
 
     it('can parse multiple log lines into a single entry', () => {
         const lines = [
-            `2019-04-23 19:16:27.905+0000 ${log} [${cls}] Something`,
+            `${date1} ${log} [${cls}] Something`,
             'something',
             'something',
 
-            `2019-04-23 19:16:27.905+0000 ${log} [${cls}] blah`,
+            `${date2} ${log} [${cls}] blah`,
             'blah',
         ];
 
@@ -19,9 +22,11 @@ describe('Log Events', function() {
         expect(events.length).toEqual(2);
         expect(events[0].classDesignator).toEqual(cls);
         expect(events[0].logLevel).toEqual(log);
+        expect(moment.utc(date1).diff(events[0].timestamp)).toEqual(0);
 
         expect(events[1].classDesignator).toEqual(cls);
         expect(events[1].logLevel).toEqual(log);
+        expect(moment.utc(date2).diff(events[1].timestamp)).toEqual(0);
     });
 
 });
