ac4da1687d6e7c6a2775ccd01ae5aa0a26a45f6e
Moxious <moxious@oldhat.org>
2019-05-15 19:36:28 -0400

    deprecated GCMonitor component

--- src/components/performance/GCMonitor.js
+++ src/components/performance/GCMonitor.js
@@ -3,6 +3,10 @@ import CypherDataTable from '../data/CypherDataTable/CypherDataTable';
 import queryLibrary from '../../api/data/queries/query-library';
 import Explainer from '../ui/scaffold/Explainer/Explainer';
 
+/**
+ * @deprecated by the GC monitor on the overview dashboard, and by the fact
+ * that this component is ugly and hard to use.  ;)
+ */
 class GCMonitor extends Component {
     query = queryLibrary.JMX_GARBAGE_COLLECTOR.query;
     displayColumns = queryLibrary.JMX_GARBAGE_COLLECTOR.columns;
