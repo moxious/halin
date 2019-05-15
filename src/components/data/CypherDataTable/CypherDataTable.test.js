9650e1843a9e59414f446ae4322b0b25e538b9e9
Moxious <moxious@oldhat.org>
2019-05-15 18:33:46 -0400

    refactor CypherDataTable -> directory

--- src/components/data/CypherDataTable/CypherDataTable.test.js
+++ src/components/data/CypherDataTable/CypherDataTable.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CypherDataTable from './CypherDataTable';
 import uuid from 'uuid';
-import fakes from '../../testutils/fakes';
+import fakes from '../../../testutils/fakes';
 
 describe('Cypher Data Table', function() {
     const key = uuid.v4();
