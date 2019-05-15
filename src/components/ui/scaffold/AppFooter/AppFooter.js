8a7a30c72f2eda0ac5cbe526981b777a16c0fa7d
Moxious <moxious@oldhat.org>
2019-05-15 11:56:58 -0400

    extensive PF'ing

--- src/components/ui/scaffold/AppFooter/AppFooter.js
+++ src/components/ui/scaffold/AppFooter/AppFooter.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { Divider, Icon, Header, Segment } from 'semantic-ui-react';
 
 import appPkg from '../../../../package.json';
+import build from '../../../../build.json';
+import moment from 'moment';
+
 import './AppFooter.css';
+
+const linkStyle = {
+    color: 'white',
+    fontSize: '10px',
+};
 
 export default class AppFooter extends Component {
     render() {
+        console.log('build',build);
+        const buildDate = moment.utc(build.date).format("YYYY-MM-DD");
         return (
-            <div className='AppFooter'>
-                <a target='halin' href='https://github.com/moxious/halin'>Halin v{appPkg.version}</a>
-                <br/>
-                <a target='halin' href='https://neo4j.com/docs/operations-manual/current/'>Neo4j Operations Manual</a>
+            <div className='AppFooter' style={{float:'bottom'}}>               
+                <Divider horizontal inverted>
+                    <Header as='h4' style={linkStyle}>
+                        <a target='halin' href='https://neo4j.com/docs/operations-manual/current/'>Operations Manual</a>
+                    </Header>                    
+                </Divider>
+                
+                <Divider horizontal inverted>
+                    <Header as='h4' style={linkStyle}>
+                        <a target='halin' href='https://github.com/moxious/halin'>Halin v{appPkg.version}</a>
+                    </Header>                    
+                </Divider>
+
+                <p style={linkStyle}>
+                    Build {build.build}<br/>{buildDate}
+                </p>
             </div>
         );
     }
