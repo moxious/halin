64006207623f53e88cb3f755bf2636eb3f584581
Moxious <moxious@oldhat.org>
2019-05-15 09:00:30 -0400

    API entrypoint and test

---
+++ src/api/index.js
@@ -0,0 +1,28 @@
+/**
+ * This module only exists to group imports underneath of the API package and make
+ * them easier to consume.
+ */
+import HalinContext from './HalinContext';
+import kb from './knowledgebase';
+import driver from './driver';
+import ClusterMember from './cluster/ClusterMember';
+import ClusterManager from './cluster/ClusterManager';
+
+import DataFeed from './data/DataFeed';
+import queryLibrary from './data/queries/query-library';
+import neo4jDesktop from './neo4jDesktop';
+import sentry from './sentry';
+
+export default {
+    HalinContext,
+    ClusterMember,
+    ClusterManager,
+    DataFeed,
+
+    kb,
+    driver,
+    queryLibrary,
+    sentry,
+
+    neo4jDesktop,
+};
