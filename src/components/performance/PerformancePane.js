7eccaa4fcc19053512a9769e1f90bfd5c4479deb
Moxious <moxious@oldhat.org>
2019-05-16 15:40:21 -0400

    convert major inflexible grids into fluid card groups

--- src/components/performance/PerformancePane.js
+++ src/components/performance/PerformancePane.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Grid } from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
 import uuid from 'uuid';
 
 import api from '../../api/';
@@ -9,40 +9,26 @@ import MemoryMonitor from './MemoryMonitor';
 import SystemLoad from './SystemLoad';
 import TransactionMonitor from './TransactionMonitor';
 import CypherPieChart from '../data/CypherPieChart/CypherPieChart';
+import HalinCard from '../ui/scaffold/HalinCard/HalinCard';
 
 export default class PerformancePane extends Component {
     render() {
         const key = uuid.v4();
 
         return (
-            <div className="PerformancePane">
-                <Grid divided='vertically'>
-                    <Grid.Row columns={2}>
-                        <Grid.Column>
-                            <SystemLoad key={key} node={this.props.node} />
-                        </Grid.Column>
-
-                        <Grid.Column>
-                            <MemoryMonitor key={key} node={this.props.node} />
-                        </Grid.Column>
-                    </Grid.Row>
-
-                    <Grid.Row columns={2}>
-                        <Grid.Column>
-                            <TransactionMonitor key={key} node={this.props.node} />
-                        </Grid.Column>
-
-                        <Grid.Column>
-                            <CypherPieChart
-                                query={api.queryLibrary.JMX_DISK_UTILIZATION.query}
-                                member={this.props.node}
-                                title="Title"
-                                label="Disk Utilization"
-                                units="GB" />
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>  
-            </div>
+            <Card.Group itemsPerRow={2} className="PerformancePane">
+                <SystemLoad key={key} node={this.props.node} />
+                <MemoryMonitor key={key} node={this.props.node} />
+                <TransactionMonitor key={key} node={this.props.node} />
+                <HalinCard>
+                    <CypherPieChart
+                        query={api.queryLibrary.JMX_DISK_UTILIZATION.query}
+                        member={this.props.node}
+                        title="Title"
+                        label="Disk Utilization"
+                        units="GB" />
+                </HalinCard>                                
+            </Card.Group>
         );
     }
 }
