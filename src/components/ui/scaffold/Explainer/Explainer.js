f101cdc152704bcd01eae5b854d081bf516f260e
Moxious <moxious@oldhat.org>
2019-05-16 15:38:54 -0400

    cleanup and warn on caller errors

--- src/components/ui/scaffold/Explainer/Explainer.js
+++ src/components/ui/scaffold/Explainer/Explainer.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { Popup, Icon } from 'semantic-ui-react'
 import kb from '../../../../api/knowledgebase';
+import sentry from '../../../../api/sentry/';
+
+const iGotNothing = 'No further information available';
 
 export default class Explainer extends Component {
     getContent() {
         if (this.props.knowledgebase) {
-            return kb[this.props.knowledgebase];
+            if (!kb[this.props.knowledgebase]) {
+                sentry.warn('No kb entry for ',this.props.knowledgebase);
+            }
+
+            return kb[this.props.knowledgebase] || iGotNothing;
         }
 
-        return this.props.content || 'No further information available';
+        return this.props.content || iGotNothing;
     }
 
     render() {        
