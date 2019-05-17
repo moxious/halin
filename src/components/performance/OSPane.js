7530f1091e7850e1b3b9377718aaf83e264f8156
Moxious <moxious@oldhat.org>
2019-05-16 21:59:13 -0400

    cleanup, and add StorageCapacity component

--- src/components/performance/OSPane.js
+++ src/components/performance/OSPane.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import "semantic-ui-css/semantic.min.css";
 import { Grid } from 'semantic-ui-react';
-import DiskUsage from './DiskUsage';
 import PageCache from '../diagnostic/PageCache';
-// import StorageCapacity from '../diagnostic/StorageCapacity';
+import StorageCapacity from '../diagnostic/StorageCapacity';
 import OSStats from './OSStats';
 import uuid from 'uuid';
 
@@ -23,23 +22,17 @@ class OSPane extends Component {
                         </Grid.Column>
                     </Grid.Row>
 
-                    <Grid.Row>
-                        <Grid.Row>
-                            <PageCache key={`${key}1`} node={this.props.node} />
-                        </Grid.Row>
-                    
-                        {/* 
-                            TODO
-                            The following component is not yet completed or tested because it
-                            relies upon an APOC plugin that isn't finished yet.
-                            Watch this space.
+                    <Grid.Row columns={2}>
+                        <Grid.Column>
+                            <StorageCapacity key={key} node={this.props.node} />
+                        </Grid.Column>
 
-                            <Grid.Column>
-                                <StorageCapacity key={key} node={this.props.node} />
-                            </Grid.Column> 
-                        */}
+                        <Grid.Column>
+                            <PageCache key={`${key}1`} node={this.props.node} />
+                        </Grid.Column>
                     </Grid.Row>
-                    <Grid.Row columns={1}>
+                    
+                    <Grid.Row>
                     </Grid.Row>
                 </Grid>  
             </div>
