d9536f33ee65cd48e6ac10fd14347cb372e90bda
Moxious <moxious@oldhat.org>
2019-05-17 08:20:05 -0400

    remove old create forms

--- src/components/configuration/PermissionsPane.js
+++ src/components/configuration/PermissionsPane.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import './PermissionsPane.css';
 import { Grid } from 'semantic-ui-react';
 import Neo4jUsers from './users/Neo4jUsers';
-import NewUserForm from './users/NewUserForm';
 import sentry from '../../api/sentry/index';
 import Neo4jRoles from './roles/Neo4jRoles';
-import NewRoleForm from './roles/NewRoleForm';
 import Explainer from '../ui/scaffold/Explainer/Explainer';
 import uuid from 'uuid';
 
@@ -45,21 +43,6 @@ class PermissionsPane extends Component {
 
                     <Grid.Row columns={columns}>
                         <Grid.Column>
-                            <NewUserForm
-                                key={this.state.key}
-                                node={this.props.node}
-                                onUserCreate={username => this.event('user', username)} />
-                        </Grid.Column>
-                        { enterprise ? <Grid.Column>
-                            <NewRoleForm
-                                key={this.state.key}
-                                node={this.props.node}
-                                onRoleCreate={role => this.event('role', role)} />
-                        </Grid.Column> : '' }
-                    </Grid.Row>
-
-                    <Grid.Row columns={columns}>
-                        <Grid.Column>
                             <Neo4jUsers
                                 key={this.state.key}
                                 node={this.props.node}
@@ -71,12 +54,6 @@ class PermissionsPane extends Component {
                                 node={this.props.node}
                                 refresh={this.state.childRefresh} />
                         </Grid.Column> : '' }
-                    </Grid.Row>
-
-                    <Grid.Row columns={1}>
-                        <Grid.Column>
-
-                        </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </div>
