6649485ed3009a50612d3337ee3b860bc0834d54
Moxious <moxious@oldhat.org>
2019-05-16 21:34:22 -0400

    factor DiskUtilizationPieChart into separate component

---
+++ src/components/performance/DiskUtilizationPieChart/DiskUtilizationPieChart.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import api from '../../../api';
+
+import HalinCard from '../../ui/scaffold/HalinCard/HalinCard';
+import CypherPieChart from '../../data/CypherPieChart/CypherPieChart';
+import Explainer from '../../ui/scaffold/Explainer/Explainer';
+
+export default class DiskUtilizationPieChart extends Component {
+    render() {
+        return (
+            <HalinCard>
+                <h3>Disk Utilization <Explainer knowledgebase='DiskUtilization' /></h3>
+
+                <CypherPieChart
+                    query={api.queryLibrary.JMX_DISK_UTILIZATION.query}
+                    member={this.props.node}
+                    title="Title"
+                    units="GB" />
+            </HalinCard>
+        );
+    }
+};
--- src/components/performance/PerformancePane.js
+++ src/components/performance/PerformancePane.js
@@ -1,33 +1,20 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Card } from 'semantic-ui-react';
-import uuid from 'uuid';
-
-import api from '../../api/';
 
 import MemoryMonitor from './MemoryMonitor';
 import SystemLoad from './SystemLoad';
 import TransactionMonitor from './TransactionMonitor';
-import CypherPieChart from '../data/CypherPieChart/CypherPieChart';
-import HalinCard from '../ui/scaffold/HalinCard/HalinCard';
+import DiskUtilizationPieChart from './DiskUtilizationPieChart/DiskUtilizationPieChart';
 
 export default class PerformancePane extends Component {
     render() {
-        const key = uuid.v4();
-
         return (
             <Card.Group itemsPerRow={2} className="PerformancePane">
-                <SystemLoad key={key} node={this.props.node} />
-                <MemoryMonitor key={key} node={this.props.node} />
-                <TransactionMonitor key={key} node={this.props.node} />
-                <HalinCard>
-                    <CypherPieChart
-                        query={api.queryLibrary.JMX_DISK_UTILIZATION.query}
-                        member={this.props.node}
-                        title="Title"
-                        label="Disk Utilization"
-                        units="GB" />
-                </HalinCard>                                
+                <SystemLoad node={this.props.node} />
+                <MemoryMonitor node={this.props.node} />
+                <TransactionMonitor node={this.props.node} />
+                <DiskUtilizationPieChart node={this.props.node} />
             </Card.Group>
         );
     }
