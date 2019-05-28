37fea402b4cc89b2df1bf40ba6341168af623ac0
Moxious <moxious@oldhat.org>
2019-05-28 11:56:53 -0400

    color ordering swap for better visual clarity

--- src/api/palette.js
+++ src/api/palette.js
@@ -1,5 +1,5 @@
 const DEFAULT_PALETTE = [
-    '#f68b24', 'steelblue', '#619F3A', '#dfecd7', '#e14594', '#7045af', '#2b3595',
+    '#f68b24', 'steelblue', '#619F3A', '#e14594', '#7045af', '#2b3595', '#dfecd7', 
 ];
 
 let spinCounter = -1;
