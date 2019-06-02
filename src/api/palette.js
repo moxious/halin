8b252c43018cb88b0282743c5c0b31336f811e93
Moxious <moxious@oldhat.org>
2019-06-02 16:38:45 -0400

    incorporate two-tone palette for allocation charts

--- src/api/palette.js
+++ src/api/palette.js
@@ -2,6 +2,12 @@ const DEFAULT_PALETTE = [
     '#f68b24', 'steelblue', '#619F3A', '#e14594', '#7045af', '#2b3595', '#dfecd7', 
 ];
 
+// https://www.colourlovers.com/palette/612903/Two_KneeOns
+const TWO_TONE = [
+    '#35CF21', // Limed
+    '#6814DF', // Gorilla Grape
+];
+
 let spinCounter = -1;
 
 const chooseColor = (k = (++spinCounter), palette=DEFAULT_PALETTE) =>
@@ -10,4 +16,5 @@ const chooseColor = (k = (++spinCounter), palette=DEFAULT_PALETTE) =>
 export default {
     chooseColor,
     DEFAULT_PALETTE,
+    TWO_TONE,
 };
