8740755391511ff4b9d4c0230ee364631de13f41
Moxious <moxious@oldhat.org>
2019-06-02 15:02:30 -0400

    add optional intro information to Explainer

--- src/components/ui/scaffold/Explainer/Explainer.js
+++ src/components/ui/scaffold/Explainer/Explainer.js
@@ -6,7 +6,7 @@ import sentry from '../../../../api/sentry/';
 const iGotNothing = 'No further information available';
 
 export default class Explainer extends Component {
-    getContent() {
+    getMainContent() {
         if (this.props.knowledgebase) {
             if (!kb[this.props.knowledgebase]) {
                 sentry.warn('No kb entry for ',this.props.knowledgebase);
@@ -16,6 +16,21 @@ export default class Explainer extends Component {
         }
 
         return this.props.content || iGotNothing;
+    }
+
+    getIntroContent() {
+        return this.props.intro || null;
+    }
+
+    getContent() {
+        const intro = this.getIntroContent();
+        const mainContent = this.getMainContent();
+
+        return (intro ? 
+            <div>
+                { intro }
+                { mainContent }
+            </div> : mainContent);
     }
 
     render() {        
