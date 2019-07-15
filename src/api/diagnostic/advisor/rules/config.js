8c5562ebe9fbdb5262942216403423c6f147f6ad
Moxious <moxious@oldhat.org>
2019-07-15 09:17:54 -0400

    progress on #102 security rules

--- src/api/diagnostic/advisor/rules/config.js
+++ src/api/diagnostic/advisor/rules/config.js
@@ -1,6 +1,28 @@
 import Advice from '../Advice';
 import _ from 'lodash';
 
+// Helper function which checks a config option across all nodes in the package.
+// Calls ruleFunction with addr, configValue, and node.
+const mapNodeAndConfigOption = (pkg, configOption, adviceGenerator) =>
+    pkg.nodes.map(node => {
+        const val = node.configuration[configOption];
+        const addr = node.basics.address;
+
+        return adviceGenerator(addr, val, node);
+    });
+
+// 2.1 Ensure that authentication is enabled for Neo4j databases
+// Description:
+// This setting ensures that all clients, users, and/or servers are required to authenticate
+// prior to being granted access to the Neo4j database.
+// 
+// Rationale:
+// Failure to authenticate clients, users, and/or servers can enable unauthorized access to the
+// Neo4j database and can prevent tracing actions back to their sources.
+// 
+// Remediation:
+// The authentication mechanism should be implemented before anyone accesses the
+// Neo4j Server.
 const authEnabled = pkg => {
     const findings = [];
 
@@ -40,6 +62,71 @@ const authEnabled = pkg => {
     return findings;
 };
 
+// 2.3 Use Neo4j with an LDAP Auth Provider
+// Description:
+// Configuring Neo4j to use an LDAP Auth provider offloads management of federated users to the LDAP service. The LDAP service facilities are used for administration. 
+// 
+// Rationale:
+// An LDAP auth provider provides stronger user management (disable users, password changes, password complexity, etc.) than available with Neo4j native users. It also streamlines the user management across a cluster, as Neo4j does not centralize management of native users.
+// 
+// Remediation:
+// The authentication mechanism should be implemented before anyone accesses the
+// Neo4j Server.
+const useLDAP = pkg => {
+    const adviceGenerator = (addr, authProvider, node) => {
+        if (authProvider !== 'ldap') {
+            return Advice.info({
+                addr, 
+                finding: 'You are not using an LDAP auth provider.',
+                advice: `Consider using an LDAP provider.  
+                    An LDAP auth provider provides stronger user management (disable users, 
+                    password changes, password complexity, etc.) than available with Neo4j 
+                    native users. It also streamlines the user management across a cluster, as 
+                    Neo4j does not centralize management of native users.
+                `,
+            });
+        }
+
+        return Advice.pass({
+            addr, finding: 'You are using the LDAP auth provider.  Good!',
+        });
+    };
+
+    return mapNodeAndConfigOption(pkg, 'dbms.security.auth_provider', adviceGenerator);
+};
+
+// 2.5 Configure User Authentication Maximum Failed Attempts
+// Description:
+// When using Neo4j native user management, configure the maximum failed attempts to guard against brute force attacks.
+// 
+// Rationale:
+// Configuring the maximum failed attempts will help minimize the chance of a successful brute force attack (avoid an attacker guessing a password by constantly requesting authentication to the server).
+// 
+// Remediation:
+// In the Neo4j.conf file, add the dbms.security.auth_max_failed_attempts configuration parameter:
+// 
+// dbms.security.auth_max_failed_attempts=10
+// 
+// The default value for this parameter is 3.
+const configureMaxFailedAttempts = pkg => {
+    const adviceGenerator = (addr, maxFailedAttempts, node) => {
+        if (maxFailedAttempts && maxFailedAttempts >= 3 && maxFailedAttempts <= 10) {
+            return Advice.pass({
+                addr,
+                finding: 'You have a maximum number of failed user auth attempts configured.  Good!',
+            });
+        }
+
+        return Advice.warn({
+            addr,
+            finding: `Your dbms.security.auth_max_failed_attempts is set to '${maxFailedAttempts}'`,
+            advice: 'Set this to a value between 3 and 10 to minimize the chances of brute force password attack',
+        });
+    };
+
+    return mapNodeAndConfigOption(pkg, 'dbms.security.auth_max_failed_attempts', adviceGenerator);
+};
+
 const overloading = pkg => {
     const findings = [];
 
@@ -66,5 +153,6 @@ const overloading = pkg => {
 };
 
 export default [
-    overloading, authEnabled,
+    overloading, authEnabled, useLDAP,
+    configureMaxFailedAttempts,
 ];
