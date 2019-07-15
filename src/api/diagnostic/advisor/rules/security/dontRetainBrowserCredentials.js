68ebede03cb25d4419267de4c406119bdf8d4725
Moxious <moxious@oldhat.org>
2019-07-15 17:53:35 -0400

    progress on #102

--- src/api/diagnostic/advisor/rules/security/dontRetainBrowserCredentials.js
+++ src/api/diagnostic/advisor/rules/security/dontRetainBrowserCredentials.js
@@ -1,6 +1,17 @@
 import metarule from '../metarule';
 import Advice from '../../Advice';
 
+// 4.1 Ensure Neo4j Browser does not cache login information
+// 
+// Description:
+// The Neo4j browser requires a username / password for login. This setting 
+// ensures that all clients using the Neo4j browser are required to login 
+// each time and that the login credentials are not maintained.
+// 
+// Rationale:
+// Enforcing a login each time prevents unauthorized access to the Neo4j 
+// database and provides traceability of database activities to a specific 
+// user or component.
 export default (pkg) =>
     metarule.forConfigOption(pkg, 'browser.retain_connection_credentials', (addr, val, node) => {
         if(val === 'false') {
--- src/api/diagnostic/advisor/rules/security/index.js
+++ src/api/diagnostic/advisor/rules/security/index.js
@@ -1,10 +1,13 @@
 import configureAuthLockoutTime from './configureAuthLockoutTime';
 import configureMaxFailedAttempts from './configureMaxFailedAttempts';
+import configureCORSHeaders from './configureCORSHeaders';
 import dontRetainBrowserCredentials from './dontRetainBrowserCredentials';
 import enableAuth from './enableAuth';
+import implementBrowserTimeout from './implementBrowserTimeout';
 import recommendDisableHTTP from './recommendDisableHTTP';
 import recommendNoExternalJMX from './recommendNoExternalJMX';
 import requireTLSForBolt from './requireTLSforBolt';
+import setHSTSResponseHeader from './setHSTSResponseHeader';
 import useLDAP from './useLDAP';
 
 export default [
@@ -12,8 +15,11 @@ export default [
     recommendDisableHTTP,
     recommendNoExternalJMX,
     configureAuthLockoutTime,
+    configureCORSHeaders,
     enableAuth,
+    implementBrowserTimeout,
     configureMaxFailedAttempts,
     requireTLSForBolt,
+    setHSTSResponseHeader,
     useLDAP,
 ];
