fde665169edb26d30e6f7e8c1a3049812d3df1d1
Moxious <moxious@oldhat.org>
2019-07-31 15:14:04 -0600

    tabbed layout

--- src/components/configuration/PermissionsPane/PermissionsPane.js
+++ src/components/configuration/PermissionsPane/PermissionsPane.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './PermissionsPane.css';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Tab } from 'semantic-ui-react';
 import Neo4jUsers from '../users/Neo4jUsers/Neo4jUsers';
 import sentry from '../../../api/sentry/index';
 import Neo4jRoles from '../roles/Neo4jRoles/Neo4jRoles';
@@ -24,6 +24,49 @@ class PermissionsPane extends Component {
     }
 
     render() {
+        const paneOf = stuff => <Tab.Pane>{stuff}</Tab.Pane>;
+
+        const userPane = {
+            menuItem: 'Users',
+            render: () => paneOf(
+                <Neo4jUsers
+                    key={this.state.key}
+                    node={this.props.node}
+                    refresh={this.state.childRefresh} />),
+        };
+
+        const rolesPane = {
+            menuItem: 'Roles',
+            render: () => paneOf(
+                <Neo4jRoles
+                    key={this.state.key}
+                    node={this.props.node}
+                    refresh={this.state.childRefresh} />
+            ),
+        };
+
+        const privilegesPane = {
+            menuItem: 'Privileges',
+            render: () => paneOf(
+                <PrivilegesTable key={this.state.key}
+                    node={this.props.node}
+                    refresh={this.state.childRefresh} />
+            ),
+        };
+
+        const panes = [userPane];
+        const enterprise = window.halinContext.isEnterprise();
+        const is4 = window.halinContext.getVersion().major >= 4;
+
+        if (enterprise) { panes.push(rolesPane); }
+        if (enterprise && is4) { panes.push(privilegesPane); }
+
+        return (
+            <Tab panes={panes} className='PermissionsPane' />
+        );
+    }
+
+    __render() {
         // Community doesn't have roles.  So based on this page's layout, the number
         // of columns and components we need depends on if we're enterprise or not.
         // The concept of applying roles to users doesn't work in Community.
@@ -36,7 +79,7 @@ class PermissionsPane extends Component {
                     <Grid.Row columns={1}>
                         <Grid.Column>
                             <h3>Permissions Management
-                                <Explainer knowledgebase='UserManagement'/>
+                                <Explainer knowledgebase='UserManagement' />
                             </h3>
                         </Grid.Column>
                     </Grid.Row>
@@ -48,12 +91,12 @@ class PermissionsPane extends Component {
                                 node={this.props.node}
                                 refresh={this.state.childRefresh} />
                         </Grid.Column>
-                        { enterprise ? <Grid.Column>
+                        {enterprise ? <Grid.Column>
                             <Neo4jRoles
                                 key={this.state.key}
                                 node={this.props.node}
                                 refresh={this.state.childRefresh} />
-                        </Grid.Column> : '' }
+                        </Grid.Column> : ''}
                     </Grid.Row>
 
                     <Grid.Row>
