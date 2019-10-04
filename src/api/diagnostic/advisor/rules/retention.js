622cbd187ff2c8503997553b8adb6e738e7ed230
Moxious <moxious@oldhat.org>
2019-10-04 08:13:46 -0400

    fix finding missing parameters

--- src/api/diagnostic/advisor/rules/retention.js
+++ src/api/diagnostic/advisor/rules/retention.js
@@ -1,15 +1,16 @@
-import InspectionResult from '../InspectionResult';
+import Advice from '../Advice';
 
 const logRetention = pkg => {
     const findings = [];
- 
-    pkg.nodes.forEach(node => {        
+
+    pkg.nodes.forEach(node => {
         const addr = node.basics.address;
         const val = node.configuration['dbms.tx_log.rotation.retention_policy'];
 
         if (!val || val === '7 days') {
-            findings.push(new InspectionResult(InspectionResult.WARN, addr,
-                    `The log rotation policy is set to the default; TX logs can become
+            findings.push(Advice.warn({
+                addr,
+                finding: `The log rotation policy is set to the default; TX logs can become
                     large, and this may contribute to full disk errors.
 
                     Generally speaking, you should set this parameter to slightly larger than the 
@@ -18,10 +19,10 @@ const logRetention = pkg => {
                     backup we really only need to set this parameter to 1 days, or for 
                     extra safety for if/when an incremental fails set it to 2 days
                     `,
-                    'Consult https://neo4j.com/docs/operations-manual/3.5/configuration/transaction-logs/ for more information',
-                ));
+                advice: 'Consult https://neo4j.com/docs/operations-manual/3.5/configuration/transaction-logs/ for more information',
+            }));
         } else {
-            findings.push(new InspectionResult(InspectionResult.PASS, addr,
+            findings.push(Advice.pass(addr,
                 'You have customized the default transaction log retention policy. That\'s good!'));
         }
     });
