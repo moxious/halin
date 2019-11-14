65c17384597cb6f363e6c41d481541f4b00fd125
Moxious <moxious@oldhat.org>
2019-11-14 14:58:36 -0500

    bare query on neo4j 4

--- src/api/data/queries/dbms/security/userRoles.js
+++ src/api/data/queries/dbms/security/userRoles.js
@@ -3,6 +3,7 @@ import HalinQuery from '../../HalinQuery';
 export default new HalinQuery({
     description: 'Gets roles for a certain user',
     dependency: null,
+    bare: true, // Do not disclaim; under Neo4j 4.0 MR3, the disclaimed query fails!
     query: `
         CALL dbms.security.listRolesForUser($username) 
         YIELD value 
