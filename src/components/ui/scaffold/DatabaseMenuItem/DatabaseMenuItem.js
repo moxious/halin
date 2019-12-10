bdd5662c66aae5c81c22ffa962c9218ee14dddaf
Moxious <moxious@oldhat.org>
2019-12-10 14:15:51 -0500

    simplify code in favor of databaseoverview component

--- src/components/ui/scaffold/DatabaseMenuItem/DatabaseMenuItem.js
+++ src/components/ui/scaffold/DatabaseMenuItem/DatabaseMenuItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Icon, Popup } from 'semantic-ui-react';
+import { Menu, Icon } from 'semantic-ui-react';
 
 export default class DatabaseMenuItem extends Component {
     state = {
@@ -10,50 +10,23 @@ export default class DatabaseMenuItem extends Component {
     statusIcon = (db) => {
         let color = 'red';
 
-        if (db.isOnline()) {
+        if (db.isReconciling()) {
+            color = 'yellow';
+        } else if (db.isOnline()) {
             color = 'green';
         }
 
         return <Icon name='database' color={color} />;
     }
 
-    popupContent = () => {
-        return (
-            <div className='PopupContent'>
-                {
-                    this.props.database.isDefault ? 
-                    <div>
-                        <Icon name='star' color='yellow' />
-                        This is the default database
-                    </div> :
-                    <div>
-                        <Icon name='star' color='grey' />
-                        Not the default database
-                    </div>
-                }
-            </div>
-        );
-    };
-
     render() {
         return (
-            <Popup
-                inverted
-                position='right center'
-                wide='very'
-                key={this.props.database.getLabel()}
-                trigger={
-                    <Menu.Item as='a'
-                        active={this.props.active}
-                        onClick={() => this.props.onSelect(this.props.database, this)}>
-                        {this.statusIcon(this.props.database)}
-                        {this.props.database.getLabel()}
-                    </Menu.Item>
-                }
-                header={`Status: ${this.props.database.getStatus()}`}
-                content={this.popupContent()}
-            />
-
+            <Menu.Item as='a'
+                active={this.props.active}
+                onClick={() => this.props.onSelect(this.props.database, this)}>
+                {this.statusIcon(this.props.database)}
+                {this.props.database.getLabel()}
+            </Menu.Item>
         );
     }
 }
