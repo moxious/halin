8f0b6bdd0ade88564606c41231146ed06f04d5eb
Moxious <moxious@oldhat.org>
2019-12-10 10:02:42 -0500

    revamping how status is assigned

--- src/api/Database.js
+++ src/api/Database.js
@@ -1,55 +1,125 @@
 import _ from 'lodash';
-
+import sentry from '../api/sentry';
 /**
  * A database is a graph that can be stored within Neo4j.
  * 
  * Multi-database starts with Neo4j >= 4.0.  For versions of Neo4j prior to 4,
  * the HalinContext will fake a single database.
+ * 
+ * Neo4j doesn't have a clustered abstraction of a database, instead there is a cluster
+ * member database, and to understand database status, you have to look at all the members
+ * and understand the net of that situation.
+ * 
+ * Docs on database state: https://neo4j.com/docs/operations-manual/4.0-preview/monitoring/individual-db-states/
  */
 
 export default class Database {
-    constructor(obj) {
+    constructor(arrOfResults) {
+        if (!_.isArray(arrOfResults)) {
+            throw new Error('Databases must be initialized with an array of results from SHOW DATABASES');
+        }
+
         const keys = ['name', 'address', 'role', 'requestedStatus', 'currentStatus',
             'default', 'error'];
 
-        keys.forEach(k => {
-            if (_.isNil(obj[k])) {
-                throw new Error(`Initializing Database objects requires key ${k}`);
-            }
+        
+        arrOfResults.forEach(obj => {
+            keys.forEach(k => {
+                if (_.isNil(obj[k])) {
+                    throw new Error(`Initializing Database objects requires key ${k}`);
+                }
+            });
         });
 
-        this.name = obj.name;
-        this.requestedStatus = obj.requestedStatus;
-        this.currentStatus = obj.currentStatus;
-        this.default = obj.default;
-        this.error = obj.error;
-        this.address = obj.address;
-        this.role = obj.role;
+        // All records must pertain to the same database name, otherwise we're mixing
+        // inputs and you get nonsense.
+        const names = _.uniqBy('name', arrOfResults);
+        if (names.length > 1) {
+            throw new Error('Inconsistent multi-name array of results');
+        }
+
+        this.name = arrOfResults[0].name;
+        this.backingStatuses = arrOfResults;
     }
 
     asJSON() {
         return {
             name: this.name,
-            address: this.address,
-            role: this.role,
-            default: this.default,
-            requestedStatus: this.requestedStatus,
-            currentStatus: this.currentStatus,
-            error: this.error,
+            statuses: this.backingStatuses,
         };
+    }
+
+    static pre4DummyDatabase() {
+        return new Database([{
+            name: 'neo4j',
+            currentStatus: 'online',
+            requestedStatus: 'online',
+            default: true,
+            error: null,
+            address: '',
+        }]);
+    }
+
+    static fromArrayOfResults(arr) {
+        // Note that SHOW DATABASES does not return 1 row per database. 
+        // Instead, it returns 1 row per database, per Neo4j instance in the cluster. 
+        // Therefore, if you have a 4-instance cluster, hosting 3 databases, you will
+        // have 12 rows.
+        // https://neo4j.com/docs/operations-manual/4.0-preview/monitoring/individual-db-states/
+        const dbs = _.groupBy(arr, 'name');
+
+        const resultingDBs = [];
+        Object.keys(dbs).forEach(dbName => {
+            // This is an array of results grouped by name.
+            resultingDBs.push(new Database(dbs[dbName]));
+        });
+
+        console.log(resultingDBs);
+        return resultingDBs;
     }
 
     getLabel() {
         return this.name;
     }
 
+    /**
+     * If currentStatus and requestedStatus do not match on any cluster member, the database
+     * is currently reconciling or errored.
+     */
     isReconciling() {
-        return this.currentStatus !== this.requestedStatus;
+        let reconciling = false;
+        this.backingStatuses.forEach(s => {
+            reconciling = reconciling || (this.currentStatus !== this.requestedStatus);
+        });
+
+        return reconciling;
+    }
+
+    hasError() {
+        return this.backingStatuses.filter(e => e.error).length > 0;
+    }
+
+    getStatuses() {
+        return this.backingStatuses.map(s => s.currentStatus);
     }
 
     getStatus() {
-        return this.currentStatus;
+        const statuses = this.getStatuses();
+
+        if (statuses.length === 1) { return statuses[0]; }
+
+        sentry.warn('DB has multiple statuses', this.asJSON());
+        return statuses[0];
     }
 
-    isOnline() { return this.currentStatus === 'online'; }
+    isOnline() { 
+        const statuses = this.getStatuses();
+        const uniq = _.uniq(statuses);
+
+        if (uniq.length === 1 && uniq[0] === 'online') { 
+            return true;
+        }
+
+        return false;
+    }
 }
