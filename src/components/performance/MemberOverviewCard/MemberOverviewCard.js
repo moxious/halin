f850ea91178bfbbd852da5b1b74ecead1605d636
Moxious <moxious@oldhat.org>
2019-12-11 09:40:11 -0500

    move old context popup information to member overview (freshness)

--- src/components/performance/MemberOverviewCard/MemberOverviewCard.js
+++ src/components/performance/MemberOverviewCard/MemberOverviewCard.js
@@ -1,41 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import HalinCard from '../../ui/scaffold/HalinCard/HalinCard';
-import { List } from 'semantic-ui-react';
+import { List, Grid } from 'semantic-ui-react';
+import util from '../../../api/data/util.js';
 
-const MemberOverviewCard = (props) => {
-    const roles = props.member.getDatabaseRoles();
-    const databases = Object.keys(roles);
+import SignalMeter from '../../data/SignalMeter/SignalMeter';
 
-    return (
-        <HalinCard owner={this}>
-            <h3>Member Overview</h3>
+class MemberOverviewCard extends Component {
+    state = {
+        currentState: null,
+        score: 1,
+        total: 1,
+        fresh: 1,
+        notFresh: 0,
+        performance: { observations: [] },
+    };
 
-            <List style={{ textAlign: 'left' }}>
-                {databases.map((dbName, k) =>
-                    <List.Item key={k}>
-                        <strong>{dbName}</strong>: {roles[dbName]}
-                    </List.Item>)}
-            </List>
+    sampleFeeds() {
+        if (!this.mounted) { return null; }
+        const score = this.props.member.getHealthScore(window.halinContext);
+        this.setState(score);
+    }
 
-            <List style={{ textAlign: 'left' }}>
-                <List.Item>
-                    <List.Icon name='tags' />
-                    <List.Content>{props.member.id}</List.Content>
-                </List.Item>
+    componentDidMount() {
+        this.mounted = true;
+        this.interval = setInterval(() => this.sampleFeeds(), 500);
+    }
 
-                {props.member.groups && props.member.groups.length > 0 ? <List.Item>
-                    <List.Icon name='group' />
-                    <List.Content>{props.member.groups}</List.Content>
-                </List.Item> : ''}
+    componentWillUnmount() {
+        this.mounted = false;
+        clearInterval(this.interval);
+    }
 
-                <List.Item>
-                    <List.Icon name='address book' />
-                    <List.Content>{props.member.addresses.join(', ')}</List.Content>
-                </List.Item>
-            </List>
-        </HalinCard>
-    );
+    render() {
+        const roles = this.props.member.getDatabaseRoles();
+        const databases = Object.keys(roles);
+        databases.sort();
+
+        return (
+            <HalinCard owner={this}>
+                <h3>Member Overview</h3>
+
+                <List style={{ textAlign: 'left' }}>
+                    {databases.map((dbName, k) =>
+                        <List.Item key={k}>
+                            <strong>{dbName}</strong>: {roles[dbName]}
+                        </List.Item>)}
+                </List>
+
+                <List style={{ textAlign: 'left' }}>
+                    <List.Item>
+                        <List.Icon name='tags' />
+                        <List.Content>{this.props.member.id}</List.Content>
+                    </List.Item>
+
+                    {this.props.member.groups && this.props.member.groups.length > 0 ? <List.Item>
+                        <List.Icon name='group' />
+                        <List.Content>{this.props.member.groups}</List.Content>
+                    </List.Item> : ''}
+
+                    <List.Item>
+                        <List.Icon name='address book' />
+                        <List.Content>{this.props.member.addresses.join(', ')}</List.Content>
+                    </List.Item>
+                </List>
+
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={2}>
+                            <SignalMeter strength={util.signalStrengthFromFreshRatio(this.state.fresh, this.state.total)} />
+                        </Grid.Column>
+                        <Grid.Column width={14}>
+                            <p>{`${this.state.fresh} of ${this.state.total} fresh`};&nbsp;
+                                {this.state.performance.observations.length} observations; mean response
+                                &nbsp;{util.roundToPlaces(this.state.performance.mean, 0)}ms with stdev {util.roundToPlaces(this.state.performance.stdev, 0)}ms</p>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </HalinCard>
+        );
+    }
 }
 
 MemberOverviewCard.props = {
