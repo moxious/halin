34bf72f913ceb9326554b61877f5ae14acb40e08
Moxious <moxious@oldhat.org>
2019-12-11 08:19:03 -0500

    stubbing out core information on the card

--- src/components/performance/MemberOverviewCard/MemberOverviewCard.js
+++ src/components/performance/MemberOverviewCard/MemberOverviewCard.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HalinCard from '../../ui/scaffold/HalinCard/HalinCard';
+import { List } from 'semantic-ui-react';
 
 const MemberOverviewCard = (props) => {
+    const roles = props.member.getDatabaseRoles();
+    const databases = Object.keys(roles);
+
     return (
         <HalinCard owner={this}>
-            Member Overview
-        </HalinCard>        
+            <h3>Member Overview</h3>
+
+            <List style={{ textAlign: 'left' }}>
+                {databases.map((dbName, k) =>
+                    <List.Item key={k}>
+                        <strong>{dbName}</strong>: {roles[dbName]}
+                    </List.Item>)}
+            </List>
+
+            <List style={{ textAlign: 'left' }}>
+                <List.Item>
+                    <List.Icon name='tags' />
+                    <List.Content>{props.member.id}</List.Content>
+                </List.Item>
+
+                {props.member.groups && props.member.groups.length > 0 ? <List.Item>
+                    <List.Icon name='group' />
+                    <List.Content>{props.member.groups}</List.Content>
+                </List.Item> : ''}
+
+                <List.Item>
+                    <List.Icon name='address book' />
+                    <List.Content>{props.member.addresses.join(', ')}</List.Content>
+                </List.Item>
+            </List>
+        </HalinCard>
     );
 }
 
