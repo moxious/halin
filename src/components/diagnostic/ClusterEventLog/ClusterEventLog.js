c0eb5c9c02def41a29f1d83dc04e61f3d89e068b
Moxious <moxious@oldhat.org>
2019-12-12 15:29:39 -0500

    event log can be downloaded as CSV

--- src/components/diagnostic/ClusterEventLog/ClusterEventLog.js
+++ src/components/diagnostic/ClusterEventLog/ClusterEventLog.js
@@ -3,12 +3,16 @@ import ReactTable from 'react-table';
 import _ from 'lodash';
 import moment from 'moment';
 
+import Explainer from '../../ui/scaffold/Explainer/Explainer';
+import CSVDownload from '../../data/download/CSVDownload';
+
 class ClusterEventLog extends Component {
     state = {
         displayColumns: [
             { 
                 Header: 'Date', 
                 Cell: e => moment(e.date).format(),
+                accessor: 'date',
             },
             {
                 Header: 'Type',
@@ -33,9 +37,15 @@ class ClusterEventLog extends Component {
 
         return (
             <div className='ClusterEventLog'>
-                <h3>Event Log</h3>
+                <h3>Event Log <Explainer knowledgebase='EventLog' /></h3>
 
-                <ReactTable className='-striped -highlight'
+                <CSVDownload 
+                    title="Results"
+                    data={events}
+                    displayColumns={this.state.displayColumns}
+                    filename={`Halin-event-log-${moment.utc().format()}.csv`} />
+
+                <ReactTable style={{marginTop:15}} className='-striped -highlight'
                     data={events}
                     sortable={true}
                     filterable={true}
