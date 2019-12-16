63bb9bb56fddea186d24e53dca888077c2b6fc43
Moxious <moxious@oldhat.org>
2019-12-16 08:50:30 -0500

    database status has pop-up context information

--- src/components/ui/scaffold/DatabaseStatusIcon/DatabaseStatusIcon.js
+++ src/components/ui/scaffold/DatabaseStatusIcon/DatabaseStatusIcon.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Popup, List } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 class DatabaseStatusIcon extends Component {
     state = {
         currentState: null,
         status: 'online',
     };
+
+    popupContent() {
+        return (
+            <div>
+                {this.statusText()}
+                {this.isDefault()}
+                {this.errors()}
+            </div>
+        )
+    }
+
+    isDefault() {
+        if (this.props.db.isDefault()) {
+            return (<p><strong>This is the default database</strong></p>);
+        }
+
+        return '';
+    }
+
+    errors() {
+        if (this.props.db.hasError()) {
+            const errors = this.props.db.getErrors();
+            const addrs = Object.keys(errors);
+
+            return (<div>
+                <strong>Reported errors:</strong>
+                <List>
+                    { addrs.map((addr, i) => 
+                        <List.Item key={i}>
+                            {addr}: {errors[addr]}
+                        </List.Item>) }
+                </List>
+            </div>);
+        }
+
+        return <p>No reported errors</p>;
+    }
+
+    statusText() {       
+        if (this.props.db.isOnline()) {
+            return <p>Current Status: <strong>ONLINE</strong></p>            
+        }
+
+        return <p>Cluster is transitioning; reported statuses: <em>{this.props.db.getStatuses().join(', ')}</em></p>
+    }
 
     render() {
         let color = 'red';
@@ -21,7 +67,11 @@ class DatabaseStatusIcon extends Component {
             color = 'green';
         }
 
-        return <Icon name='database' color={color} />;
+        return (
+            <Popup content={this.popupContent()} trigger={
+                <Icon name='database' color={color} {..._.pick(this.props, ['size'])}/>
+            }/>
+        );
     }
 };
 
