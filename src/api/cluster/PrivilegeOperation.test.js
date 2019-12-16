9eeaa07a2148aabf9df9b13af2889b1ac0624497
Moxious <moxious@oldhat.org>
2019-12-16 11:43:50 -0500

    cover reverseability of privileges

--- src/api/cluster/PrivilegeOperation.test.js
+++ src/api/cluster/PrivilegeOperation.test.js
@@ -72,4 +72,79 @@ describe('Privilege Operations', function() {
 
         expect(op.buildQuery()).toEqual('GRANT CREATE INDEX ON DATABASE foo TO blorko');        
     });
+
+    it('allowsEntity for non-database privileges', () => {
+        expect(PrivilegeOperation.allowsEntity(PrivilegeOperation.PRIVILEGES.READ_ALL)).toBeTruthy();
+        expect(PrivilegeOperation.allowsEntity(PrivilegeOperation.PRIVILEGES.TRAVERSE)).toBeTruthy();
+    });
+
+    it('does not allowEntity for WRITE or Database privileges', () => {
+        expect(PrivilegeOperation.allowsEntity(PrivilegeOperation.DATABASE_OPERATIONS.ACCCESS)).toBeFalsy();
+        expect(PrivilegeOperation.allowsEntity(PrivilegeOperation.PRIVILEGES.WRITE)).toBeFalsy();
+    });
+
+    /**
+     * These scenarios are to work out various forms of query generation that needs to
+     * happen, and the various quirks of the system command syntax.
+     */
+    const scenarios = [
+        {
+            reversalAction: 'DENY',
+            op: {
+                access: 'GRANTED',
+                action: 'access',
+                resource: 'database',
+                graph: 'neo4j',
+                segment: 'database',
+                role: 'test',
+            },
+            expected: 'DENY ACCESS ON DATABASE neo4j TO test',
+        },
+        {
+            reversalAction: 'DENY',
+            op: {
+                access: 'GRANTED',
+                action: 'traverse',
+                resource: 'graph',
+                graph: 'neo4j',
+                segment: 'NODE(*)',
+                role: 'test',
+            },
+            expected: 'DENY TRAVERSE ON GRAPH neo4j NODES * TO test',
+        },
+        {
+            reversalAction: 'REVOKE',
+            op: {
+                access: 'GRANTED',
+                action: 'create_propertykey',
+                resource: 'database',
+                graph: 'neo4j',
+                segment: 'database',
+                role: 'test',
+            },
+            expected: 'REVOKE CREATE NEW PROPERTY NAME ON DATABASE neo4j FROM test',
+        },
+        {
+            reversalAction: 'REVOKE',
+            op: {
+                access: 'GRANTED',
+                action: 'read',
+                resource: 'all_properties',
+                graph: 'neo4j',
+                segment: 'RELATIONSHIP(*)',
+                role: 'test',
+            },
+            expected: 'REVOKE READ {*} ON GRAPH neo4j RELATIONSHIPS * FROM test',
+        },
+    ];
+
+    scenarios.forEach(scenario => {
+        it(`Should ${scenario.expected} given ${scenario.reversalAction} of an operation`, () => {
+            const op = PrivilegeOperation.fromSystemPrivilege(
+                scenario.reversalAction,
+                scenario.op);
+            
+                expect(op.buildQuery()).toEqual(scenario.expected);
+        });
+    });
 });
