39bb1076ea8cf2b9f01318e6d3945710bce202ec
Moxious <moxious@oldhat.org>
2019-12-17 09:31:07 -0500

    first commit

---
+++ src/api/data/queries/jmx/4.0/totalStoreSize.js
@@ -0,0 +1,24 @@
+import HalinQuery from '../../HalinQuery';
+
+export default new HalinQuery({
+    description: 'Fetches total size of the database as stored on disk',
+    // otherStore is a calculated value that catches all other files which may
+    // be in the store directory which don't belong to Neo4j.
+    query: `
+        CALL dbms.queryJmx("neo4j.metrics:name=neo4j." + $db + ".store.size.total") 
+        YIELD attributes 
+        RETURN attributes.Value.value AS sizeInBytes
+    `,
+    parameters: {
+        db: 'Database name',
+    },
+    columns: [
+        { Header: 'Size in Bytes', accessor: 'sizeInBytes' },
+    ],
+    rate: 1000,
+    exampleResult: [
+        {
+            sizeInBytes: 1024,
+        },
+    ],
+});
