f463a00fbb8c3c3272bb5f906a4ca99811e5d758
Moxious <moxious@oldhat.org>
2020-01-23 12:58:48 -0500

    connectionRefused

--- src/api/driver/errors.js
+++ src/api/driver/errors.js
@@ -11,6 +11,7 @@ export default {
     insecureWSFromHTTPS: (err) => asStr(err).toLowerCase().indexOf('insecure websocket connection may not be initiated from a page loaded over HTTPS') > -1,
     repeatedAuthFailure: (err) => asStr(err).toLowerCase().indexOf('incorrect authentication details too many times in a row') > -1,
     fileNotFound: (err) => asStr(err).indexOf('java.io.FileNotFoundException') > -1,
+    connectionRefused: (err) => asStr(err).indexOf('ERR_CONNECTION_REFUSED') > -1,
     apocFileImportNotEnabled: (err) => asStr(err).indexOf('apoc.import.file.enabled') > -1,
 };
 
